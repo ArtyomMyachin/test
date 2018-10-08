0100c0a2e6f9258cd5a20a379a640ef916e28d59
Artyom Myachin <sulfuraloxid@rambler.ru>
2018-10-08 12:49:37 +0600

    Test task version 0.0

---
+++ task/scripts/script.js
@@ -0,0 +1,11 @@
+function openBar() {
+	$(".slider-column").removeClass("hidden");
+}
+
+function openModalWindow() {
+	$("#modal-window").removeClass("hidden");
+}
+
+function closeModalWindow() {
+	$("#modal-window").addClass("hidden");
+}
